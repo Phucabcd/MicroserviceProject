0608bbf8da2fd53184c38596c21168d1122eebfc
TrongPhuc <nguyentrongphuc03@gmail.com>
2025-01-11 18:07:49 +0700

    Spring Cloud Getway, API Getway Filter StripPrefix

--- identity-service/src/main/java/com/devteria/identity/entity/User.java
+++ identity-service/src/main/java/com/devteria/identity/entity/User.java
@@ -1,5 +1,6 @@
 package com.devteria.identity.entity;
 
+import java.time.LocalDate;
 import java.util.Set;
 
 import jakarta.persistence.*;
@@ -13,6 +14,7 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "[user]")
 @Entity
 public class User {
     @Id
@@ -21,6 +23,9 @@ public class User {
 
     String username;
     String password;
+    String firstName;
+    LocalDate dob;
+    String lastName;
 
     @ManyToMany
     Set<Role> roles;
